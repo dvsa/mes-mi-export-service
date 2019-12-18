d7fd056df1c9ed80584f0588d0043dcb4ee3f7ad
MarkPatt89 <m.patterson@kainos.com>
2019-12-18 14:45:04 +0000

    MES-4332: Refactoring mapper tests to reduce code and reuse object setup.

---
+++ src/functions/uploadToRSIS/application/data-mapper/__tests__/helpers/enums/function-params.ts
@@ -0,0 +1,17 @@
+export enum SeverityShortCodes {
+  DrivingFault = 'DF',
+  SeriousFault = 'S',
+  DangerousFault = 'D',
+}
+
+export enum SeverityText {
+  DrivingFault = 'fault',
+  Serious = 'serious',
+  Dangerous = 'dangerous',
+}
+
+export enum IndependentDrivingText {
+  SatNav = 'Sat nav',
+  TrafficSigns = 'Traffic signs',
+  NotApplicable = 'N/A',
+}

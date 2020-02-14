e351e8509b4f4ae81ae7bf202936beee3132e2f0
Tom Billington <t.billington@kainos.com>
2020-02-14 15:03:04 +0000

    Fixed Uncouple Recouple Comments for Cat C

--- src/functions/uploadToRSIS/application/data-mapper/cat-ce-mapper.ts
+++ src/functions/uploadToRSIS/application/data-mapper/cat-ce-mapper.ts
@@ -7,6 +7,8 @@ import {
   formatManoeuvreSerious,
   optionalBoolean,
   formatManoeuvreDangerous,
+  optional,
+  addIfSet,
 } from './data-mapper';
 
 export const mapCatCEData = (result: ResultUpload): DataField[] => {
@@ -20,6 +22,8 @@ export const mapCatCEData = (result: ResultUpload): DataField[] => {
     field('UNCOUPLE_RECOUPLE_DANGEROUS', formatManoeuvreDangerous(t, 'uncoupleRecouple.fault')),
     field('UNCOUPLE_RECOUPLE_COMPLETED', optionalBoolean(t, 'uncoupleRecouple.selected')),
   ];
+  // Optional Fields
+  addIfSet(m , 'UNCOUPLE_RECOUPLE_COMMENT', optional(t, 'uncoupleRecouple.faultComments', null));
 
   return m;
 };

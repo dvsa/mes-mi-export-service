0ca693357ac96500190447ae0af1d468e9ee3999
andrew <andrew.setterfield@dvsa.gov.uk>
2020-11-03 16:06:27 +0000

    added change for vehicleCheckCompleted for delegated tests

--- src/functions/uploadToRSIS/application/data-mapper/cat-d-mapper.ts
+++ src/functions/uploadToRSIS/application/data-mapper/cat-d-mapper.ts
@@ -8,13 +8,15 @@ import {
 } from './data-mapper';
 import { mapCommonCatDData } from './cat-d-common-mapper';
 import { CatDUniqueTypes } from '@dvsa/mes-test-schema/categories/D';
+import { get } from 'lodash';
 
 export const mapCatDData = (result: ResultUpload): DataField[] => {
   const t = result.testResult.testData as CatDUniqueTypes.TestData;
+  const delegatedTest = get(result, 'testResult.delegatedTest', false);
 
   return [
     ...mapCommonCatDData(result),
-    field('VEHICLE_CHECKS_COMPLETED', formatQuestionCompleted(t, 5)),
+    field('VEHICLE_CHECKS_COMPLETED', formatQuestionCompleted(t, 5, delegatedTest)),
     field('VEHICLE_CHECKS_TOTAL', formatQuestionFaultD(t)),
     field('VEHICLE_CHECKS_SERIOUS', formatQuestionSeriousD(t)),
   ];

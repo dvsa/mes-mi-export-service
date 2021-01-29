742db619c60db1f0a18c8384878c8ef830d1b8ca
Szabi Keresztes <szabi.keresztes@bjss.com>
2021-01-29 16:32:19 +0000

    Revert "Remove Safety competency mapping for CAT Ds"
    This reverts commit b1768eda682da049dbac7785cdf0bf69642ab01f.

--- src/functions/uploadToRSIS/application/data-mapper/__tests__/helpers/cat-de/data-fields/fully-populated-data-fields.ts
+++ src/functions/uploadToRSIS/application/data-mapper/__tests__/helpers/cat-de/data-fields/fully-populated-data-fields.ts
@@ -3,6 +3,7 @@ import { DataField } from '../../../../../../domain/mi-export-data';
 export function getCatDEFullyPopulatedDrivingDataFields(): DataField[] {
   return [
     { col: 'AUTOMATIC_TEST', val: 0 },
+    { col: 'H_CODE_SAFETY_TOTAL', val: 0 },
     { col: 'REV_LEFT_TRAIL_CONT_TOTAL', val: 1 },
     { col: 'REV_LEFT_TRAIL_OBSERV_TOTAL', val: 1 },
     { col: 'PRECAUTIONS_TOTAL', val: 2 },
@@ -201,6 +202,7 @@ export function getCatDEFullyPopulatedDrivingDataFields(): DataField[] {
 export function getCatDEFullyPopulatedSeriousDataFields(): DataField[] {
   return [
     { col: 'AUTOMATIC_TEST', val: 0 },
+    { col: 'H_CODE_SAFETY_TOTAL', val: 0 },
     { col: 'REV_LEFT_TRAIL_CONT_TOTAL', val: 0 },
     { col: 'REV_LEFT_TRAIL_OBSERV_TOTAL', val: 0 },
     { col: 'PRECAUTIONS_TOTAL', val: 0 },
@@ -400,6 +402,7 @@ export function getCatDEFullyPopulatedSeriousDataFields(): DataField[] {
 export function getCatDEFullyPopulatedDangerousDataFields(): DataField[] {
   return [
     { col: 'AUTOMATIC_TEST', val: 0 },
+    { col: 'H_CODE_SAFETY_TOTAL', val: 0 },
     { col: 'REV_LEFT_TRAIL_CONT_TOTAL', val: 0 },
     { col: 'REV_LEFT_TRAIL_OBSERV_TOTAL', val: 0 },
     { col: 'PRECAUTIONS_TOTAL', val: 0 },
